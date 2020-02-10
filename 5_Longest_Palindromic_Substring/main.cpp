fdb445eee9b6b2856d902ff2c1bcfcd363a88623
Kok Suan Lim <lks@gidcs.net>
2020-02-10 19:44:45 +0800

    add dp solution for 5_Longest_Palindromic_Substring

--- 5_Longest_Palindromic_Substring/main.cpp
+++ 5_Longest_Palindromic_Substring/main.cpp
@@ -20,6 +20,7 @@ int main(){
     input.push_back("zzzzzzzadam");
     input.push_back("zxcdfvgadam");
     input.push_back("babadada");
+    input.push_back("ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkbdhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgxoruoqrhezgsgidgcfzbtdftjxeahriirqgxbhicoxavquhbkaomrroghdnfkknyigsluqebaqrtcwgmlnvmxoagisdmsokeznjsnwpxygjjptvyjjkbmkxvlivinmpnpxgmmorkasebngirckqcawgevljplkkgextudqaodwqmfljljhrujoerycoojwwgtklypicgkyaboqjfivbeqdlonxeidgxsyzugkntoevwfuxovazcyayvwbcqswzhytlmtmrtwpikgacnpkbwgfmpavzyjoxughwhvlsxsgttbcyrlkaarngeoaldsdtjncivhcfsaohmdhgbwkuemcembmlwbwquxfaiukoqvzmgoeppieztdacvwngbkcxknbytvztodbfnjhbtwpjlzuajnlzfmmujhcggpdcwdquutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarabcroljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcudlzfzutplbycejmkpxcygsafzkgudy");
     ans.push_back("bab");
     ans.push_back("baaaab");
     ans.push_back("bab");
@@ -30,6 +31,7 @@ int main(){
     ans.push_back("zzzzzzz");
     ans.push_back("ada");
     ans.push_back("adada");
+    ans.push_back("fklkf");
     
     for(int i=0; i<ans.size(); i++){
         cout << "input: " << input[i] << endl;
--- 5_Longest_Palindromic_Substring/src/solution.cpp
+++ 5_Longest_Palindromic_Substring/src/solution.cpp
@@ -1,5 +1,155 @@
 #include "solution.hpp"
 
+#define DP_Smart
+
+#ifdef DP_Smart
+string Solution::longestPalindrome(string s){
+    if(s.size() < 1){
+        return "";
+    }
+
+    int max = 0;
+    int max_i;
+
+    for(int i=0; i<s.size(); i++){
+        int p, q, value;
+        { //case i, i
+            p = i;
+            q = i;
+            value = 1;
+            if(value > max){
+                max_i = p;
+                max = value;
+            }
+            while(--p >= 0 && ++q < s.size()){ // expand
+                if(s[p] == s[q]){
+                    value += 2;
+                }
+                else{
+                    break;
+                }
+            }
+            if(value > max){
+                max_i = p+1;
+                max = value;
+            } 
+        }
+        { // case i, i+1
+            p = i;
+            q = i+1;
+            if(q>s.size()) break; // reach end
+
+            if(s[p] == s[q]){
+                value = 2;
+                if(value > max){
+                    max_i = p;
+                    max = value;
+                }
+                while(--p >= 0 && ++q < s.size()){ // expand
+                    if(s[p] == s[q]){
+                        value += 2;
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+            if(value > max){
+                max_i = p+1;
+                max = value;
+            } 
+        }
+    }
+    return s.substr(max_i, max);
+}
+#endif
+
+#ifdef DP_Slow
+string Solution::longestPalindrome(string s){
+    if(s.size() < 1){
+        return "";
+    }
+
+    int max = 0;
+    int max_i, max_j;
+    vector<vector<int>> dict;
+    dict.resize(s.size());
+
+    //init
+    for(int i=0; i<s.size(); i++){
+        dict[i].resize(s.size());
+        for(int j=0; j<s.size(); j++){
+            dict[i][j] = -1;
+        }
+    }
+
+    for(int i=0; i<s.size(); i++){
+        for(int j=0; j<s.size(); j++){
+            if(i > j) continue;
+            int p, q, value;
+            if(i==j){
+                value = 1;
+                dict[i][j] = value;
+                p = i;
+                q = j;
+                while(--p >= 0 && ++q < s.size()){ // expand
+                    if(value !=0 && s[p] == s[q]){
+                        value += 2;
+                    }
+                    else{
+                        value = 0;
+                    }
+                    dict[p][q] = value;
+                }
+            }
+            else if(abs(i-j)==1){
+                if(s[i] == s[j]){
+                    value = 2;
+                    dict[i][j] = value;
+                    p = i;
+                    q = j;
+                    while(--p >= 0 && ++q < s.size()){ // expand
+                        if(value !=0 && s[p] == s[q]){
+                            value += 2;
+                        }
+                        else{
+                            value = 0;
+                        }
+                        dict[p][q] = value;
+                    }
+                }
+                else{
+                    value = 0;
+                    dict[i][j] = value;
+                    p = i;
+                    q = j;
+                    while(--p >= 0 && ++q < s.size()){ // expand
+                        dict[p][q] = value;
+                    }
+                }
+            }
+            else{ // expand will do the others
+                continue;
+            }
+        }
+    }
+    for(int i=0; i<s.size(); i++){
+        for(int j=0; j<s.size(); j++){
+            if(i > j) continue;
+            if(dict[i][j] > max){
+                max_i = i;
+                max_j = j;
+                max = dict[i][j];
+            }
+            //cout << "dict[" << i << "][" << j << "]: " << dict[i][j] << endl;
+        }
+    }
+    //cout << "max_i: " << max_i << " max_j:" << max_j << " max: " << max << endl;
+    return s.substr(max_i, max);
+}
+#endif
+
+#ifdef DP_Func
 int expandFromCenter(string &s, int l, int r){
     //cout << "[init] " << s << " " << l << " " << r << endl;
     while(l >=0 && r < s.size()){
@@ -50,3 +200,4 @@ string Solution::longestPalindrome(string s){
     }
     return s.substr(start, end-start+1);
 }
+#endif
